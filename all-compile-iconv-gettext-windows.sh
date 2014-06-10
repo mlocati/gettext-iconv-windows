289c4a5d1d830aab03c6a07e40504bd57ee0c142
Michele Locati <michele@locati.it>
2014-06-10 15:11:10 +0200

    Simplified handling of "build all"

--- all-compile-iconv-gettext-windows.sh
+++ all-compile-iconv-gettext-windows.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
+BLDGTXTALL_VERS_ICONV=1.14
+BLDGTXTALL_VERS_GETTEXT=0.19.1
 
-./compile-iconv-gettext-windows.sh --iconv 1.14 --gettext 0.19.1 --bits 32 --how shared
+./compile-iconv-gettext-windows.sh --iconv $BLDGTXTALL_VERS_ICONV --gettext $BLDGTXTALL_VERS_GETTEXT --bits 32 --how shared
 if [ $? -ne 0 ]; then
 	echo "Error!" >&2
 	exit 1
 fi
 
-./compile-iconv-gettext-windows.sh --iconv 1.14 --gettext 0.19.1 --bits 32 --how static
+./compile-iconv-gettext-windows.sh --iconv $BLDGTXTALL_VERS_ICONV --gettext $BLDGTXTALL_VERS_GETTEXT --bits 32 --how static
 if [ $? -ne 0 ]; then
 	echo "Error!" >&2
 	exit 1
 fi
 
-./compile-iconv-gettext-windows.sh --iconv 1.14 --gettext 0.19.1 --bits 64 --how shared
+./compile-iconv-gettext-windows.sh --iconv $BLDGTXTALL_VERS_ICONV --gettext $BLDGTXTALL_VERS_GETTEXT --bits 64 --how shared
 if [ $? -ne 0 ]; then
 	echo "Error!" >&2
 	exit 1
 fi
 
-./compile-iconv-gettext-windows.sh --iconv 1.14 --gettext 0.19.1 --bits 64 --how static
+./compile-iconv-gettext-windows.sh --iconv $BLDGTXTALL_VERS_ICONV --gettext $BLDGTXTALL_VERS_GETTEXT --bits 64 --how static
 if [ $? -ne 0 ]; then
 	echo "Error!" >&2
 	exit 1
