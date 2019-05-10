45447d72c3228600796a8411f7cca5443ea736d6
Michele Locati <michele@locati.it>
2019-05-10 17:21:24 +0200

    Check/install automake version 1.16

--- compile-iconv-gettext-windows-deps.sh
+++ compile-iconv-gettext-windows-deps.sh
@@ -64,3 +64,25 @@ else
         cc
     popd >/dev/null
 fi
+echo '### checking automake version'
+BLDGTXT_AUTOMAKE_VERSION_INSTALL='1.16.1'
+BLDGTXT_AUTOMAKE_VERSION_MIN='1.16.0'
+BLDGTXT_AUTOMAKE_VERSION_CUR="$(automake --version | head -n 1 | sed 's/^[^0-9]*//')"
+BLDGTXT_AUTOMAKE_VERSION_CHECK=$(printf '%s\n%s' "$BLDGTXT_AUTOMAKE_VERSION_CUR" "$BLDGTXT_AUTOMAKE_VERSION_MIN" | sort -t '.' -n -k1,1 -k2,2 -k3,3 -k4,4 | head -n 1)
+if test "$BLDGTXT_AUTOMAKE_VERSION_CHECK" = "$BLDGTXT_AUTOMAKE_VERSION_MIN"; then
+    printf ' - good (required: %s, found: %s)\n' "$BLDGTXT_AUTOMAKE_VERSION_MIN" "$BLDGTXT_AUTOMAKE_VERSION_CUR"
+else
+    printf ' - upgrading from %s to %s\n' "$BLDGTXT_AUTOMAKE_VERSION_CUR" "$BLDGTXT_AUTOMAKE_VERSION_INSTALL"
+    $BLDGTXT_SUDO apt-get remove --purge -y automake
+    pushd "$HOME" >/dev/null
+    wget "ftp://ftp.gnu.org/gnu/automake/automake-$BLDGTXT_AUTOMAKE_VERSION_INSTALL.tar.gz"
+    tar -xzf "automake-$BLDGTXT_AUTOMAKE_VERSION_INSTALL.tar.gz"
+    rm "automake-$BLDGTXT_AUTOMAKE_VERSION_INSTALL.tar.gz"
+    cd "automake-$BLDGTXT_AUTOMAKE_VERSION_INSTALL"
+    ./configure
+    make
+    $BLDGTXT_SUDO make install
+    cd ..
+    rm -rf "automake-$BLDGTXT_AUTOMAKE_VERSION_INSTALL"
+    popd >/dev/null
+fi
