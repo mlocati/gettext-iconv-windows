aaa581de4a704a4f054beda15eb9bf1b54c31e6c
Michele Locati <michele@locati.it>
2024-09-23 17:58:43 +0200

    Remove no more used stuff

--- patches/gettext-0.20.1-make/before.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Reduce dependency on libwinpthread
-
-sed -Ei 's/(#define HAVE_CLOCK_GETTIME 1|\/\* #undef HAVE_CLOCK_GETTIME \*\/)/#define HAVE_CLOCK_GETTIME 0/g' gettext-tools/config.h
--- patches/libiconv-1.15-configure/after.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-autoreconf --force --install -I "$PWD/srcm4" -I "$PWD/m4" --warnings=none
