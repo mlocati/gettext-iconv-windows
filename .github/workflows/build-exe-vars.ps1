a950b3169dd38bb7c8bf70349b140a96cbcb06cd
Michele Locati <michele@locati.it>
2024-09-24 10:03:35 +0200

    Be ready for next gettext versions

--- .github/workflows/build-exe-vars.ps1
+++ .github/workflows/build-exe-vars.ps1
@@ -7,6 +7,10 @@ param (
     [string] $link
 )
 
+if (-not($env:GETTEXT_VERSION)) {
+    throw 'Missing GETTEXT_VERSION environment variable'
+}
+
 $cygwinPackages = 'make,unzip,perl'
 
 $gettextIgnoreTestsC = 'gettext-tools/gnulib-tests/test-asyncsafe-spin2.c gettext-tools/gnulib-tests/test-getopt-gnu.c gettext-tools/gnulib-tests/test-getopt-posix.c'
@@ -45,6 +49,14 @@ switch ($link) {
         $gettextCppFlags = '-DLIBXML_STATIC'
     }
 }
+
+if ([Version]$env:GETTEXT_VERSION -le [Version]'0.22.5') {
+    <# See https://savannah.gnu.org/bugs/?66232 #>
+    $gettextXFailTests='msgexec-1 msgexec-3 msgexec-4 msgexec-5 msgexec-6 msgfilter-6 msgfilter-7 msginit-3'
+} else {
+    $gettextXFailTests=''
+}
+
 "cygwin-packages=$cygwinPackages" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
 "cygwin-path=/installed/bin:/usr/$mingwHost/bin:/usr/$mingwHost/sys-root/mingw/bin:/usr/sbin:/usr/bin:/sbin:/bin:/cygdrive/c/Windows/system32:/cygdrive/c/Windows" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
 "mingw-host=$mingwHost" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
@@ -52,7 +64,6 @@ switch ($link) {
 "gettext-cppflags=$gettextCppFlags" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
 <# See https://savannah.gnu.org/bugs/?66232 #>
 "gettext-ignore-tests-c=$gettextIgnoreTestsC" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
-<# See https://savannah.gnu.org/bugs/?66232 #>
-"gettext-xfail-tests=msgexec-1 msgexec-3 msgexec-4 msgexec-5 msgexec-6 msgfilter-6 msgfilter-7 msginit-3" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
+"gettext-xfail-tests=$gettextXFailTests" | Out-File -FilePath $env:GITHUB_OUTPUT -Append -Encoding utf8
 Write-Output '## Outputs'
 Get-Content $env:GITHUB_OUTPUT
