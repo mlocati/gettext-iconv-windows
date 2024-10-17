7e3d483cecf7a3b4201c5bf82fdfdbd18f0844ce
Michele Locati <michele@locati.it>
2024-10-17 16:40:53 +0200

    Add helper tool for signpath.io (#29)

---
+++ signpath/inspect-bin.ps1
@@ -0,0 +1,43 @@
+# Script dump the "product-name" and "product-version" properties .dll and .exe files
+
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateLength(1, [int]::MaxValue)]
+    [ValidateScript({Test-Path -LiteralPath $_})]
+    [string] $Path
+)
+
+function Read-File()
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo] $Path,
+        [Parameter(Mandatory = $false)]
+        [System.IO.DirectoryInfo] $RelativeTo
+
+    )
+    if ($RelativeTo) {
+        $displayName = $Path.FullName.Substring($RelativeTo.FullName.Length + 1)
+    } else {
+        $displayName = $Path.Name
+    }
+    $versionInfo = $Path.VersionInfo
+    $productName = $versionInfo ? $versionInfo.ProductName : ''
+    $productVersion = $versionInfo ? $versionInfo.ProductVersion : ''
+    Write-Output @"
+${displayName}:
+    product-name="$productName"
+    product-version="$productVersion"
+"@
+}
+
+$parent = Get-Item -LiteralPath $Path
+if ($parent.PSIsContainer) {
+    $files = Get-ChildItem -Path $Path -Recurse -File -Include *.exe,*.dll
+    $files
+    foreach ($file in $files) {
+        Read-File -Path $file -RelativeTo $parent
+    }
+} else {
+    Read-File -Path $Path
+}
