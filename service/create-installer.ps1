5d4470d7319caf08403fc78b87600d85ab215854
Michele Locati <michele@locati.it>
2026-02-12 15:09:56 +0100

    Create installers that support per-user installation

--- service/create-installer.ps1
+++ service/create-installer.ps1
@@ -35,41 +35,30 @@ function GetIssSourceFile()
     $PSCommandPath -replace '\.[^.]+$','.iss'
 }
 
-function New-IssFile()
+function Initialize-Iss()
 {
     [OutputType([string])]
+    $includeFile = $PSCommandPath -replace '\.[^.]+$','.isi'
     $templateFile = $PSCommandPath -replace '\.[^.]+$','.iss'
-    $deleteTemporaryFile = $true
-    $temporaryFile = New-TemporaryFile
-    try {
-        "#define MyVersionShownName `"$Link ($Bits bit)`"" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        "#define MyVersionCodeName `"$Link-$Bits`"" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        if ($Bits -eq 64) {
-            "#define MyIs64bit true" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        } else {
-            "#define MyIs64bit false" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        }
-        "#define MyGettextVer `"$GettextVersion`"" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        "#define MyIconvVer `"$IconvVersion`"" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        "#define MyCompiledFolderPath `"$SourceDirectory`"" | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        Get-Content -LiteralPath $templateFile -Encoding utf8 | Out-File -FilePath $temporaryFile -Append -Encoding utf8
-        $deleteTemporaryFile = $false
-        $temporaryFile
-    } finally {
-        if ($deleteTemporaryFile) {
-            Remove-Item -LiteralPath $temporaryFile -ErrorAction SilentlyContinue
-        }
+    Remove-Item -LiteralPath $includeFile -ErrorAction SilentlyContinue
+    "#define MyVersionShownName `"$Link ($Bits bit)`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    "#define MyVersionCodeName `"$Link-$Bits`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    if ($Bits -eq 64) {
+    "#define MyIs64bit true" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    } else {
+        "#define MyIs64bit false" | Add-Content -LiteralPath $includeFile -Encoding utf8
     }
+    "#define MyGettextVer `"$GettextVersion`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    "#define MyIconvVer `"$IconvVersion`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    "#define MyCompiledFolderPath `"$SourceDirectory`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    "#define MyOutputFolderPath `"$OutputDirectory`"" | Add-Content -LiteralPath $includeFile -Encoding utf8
+    return $templateFile
 }
 
 $outputFile = "gettext$GettextVersion-iconv$IconvVersion-$Link-$Bits"
-$issFile = New-IssFile
-try {
-    & ISCC.exe /O"$OutputDirectory" /F"$outputFile" "$issFile"
-    if (-not($?)) {
-        throw "ISCC.exe failed"
-    }
-    Write-Host -Object "$outputFile has been created in $OutputDirectory"
-} finally {
-    Remove-Item -LiteralPath $issFile -ErrorAction SilentlyContinue
+$issFile = Initialize-Iss
+& ISCC.exe /O"$OutputDirectory" /F"$outputFile" "$issFile"
+if (-not($?)) {
+    throw "ISCC.exe failed"
 }
+Write-Host -Object "$outputFile has been created in $OutputDirectory"
